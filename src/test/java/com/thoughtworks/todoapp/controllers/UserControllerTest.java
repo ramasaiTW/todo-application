b9708c71ebd338bb0db1d41a26b37c970daa8c66
ramasaiTW <ramasai.dhanumuri@thoughtworks.com>
2024-03-29 13:50:26 +0530

    Controller Testcases are added

---
+++ src/test/java/com/thoughtworks/todoapp/controllers/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.thoughtworks.todoapp.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.todoapp.dtos.payload.request.LoginRequest;
+import com.thoughtworks.todoapp.dtos.payload.request.SignupRequest;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@FixMethodOrder(MethodSorters.JVM)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static String email;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+    @WithMockUser("thoughtworks")
+    @Test
+    public void testRegisterUser() throws Exception {
+        int emailNumberIncrement = (int) (Math.random() * 100001);
+        email = "twtech"+emailNumberIncrement+"@example.com";
+
+        SignupRequest signupRequest = new SignupRequest("ThoughtWorks", "Technologies", email, "password");
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk()).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @WithMockUser("thoughtworks")
+    @Test
+    public void testAuthenticateUser() throws Exception {
+        LoginRequest loginRequest = new LoginRequest(email, "password");
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk()).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+}
